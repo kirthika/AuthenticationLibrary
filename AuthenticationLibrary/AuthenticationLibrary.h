4603792008446206dc4f6f64c0f408e351994cb5
kirthika <kirthika.vijayakumar@yahoo.com>
2017-06-21 17:29:00 +0530

    shared access

---
+++ AuthenticationLibrary/AuthenticationLibrary.h
@@ -0,0 +1,19 @@
+//
+//  AuthenticationLibrary.h
+//  AuthenticationLibrary
+//
+//  Created by Kirthika Vijayakumar on 21/06/17.
+//  Copyright © 2017 Kirthika Vijayakumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AuthenticationLibrary.
+FOUNDATION_EXPORT double AuthenticationLibraryVersionNumber;
+
+//! Project version string for AuthenticationLibrary.
+FOUNDATION_EXPORT const unsigned char AuthenticationLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthenticationLibrary/PublicHeader.h>
+
+
